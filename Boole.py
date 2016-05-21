48540a8955273e057adac920ec10d4a3cd60acdf
Bencake <bencake85@gmail.com>
2016-05-21 22:37:20 +0200

    MAJ Python3
    Adapté à Python3
    Code à poursuivre

--- Boole.py
+++ Boole.py
@@ -14,19 +14,19 @@ def booleNett(phrase):
     phrase_sans_op=phrase.replace("|ET|","")
     phrase_sans_op=phrase_sans_op.replace("|OU|","")
     if "|" in phrase_sans_op:
-        print "Présence de caractères \'|\' indésirables"
+        print("Présence de caractères \'|\' indésirables")
         return None
 
     # 2.2-Vérification du bon nombre de parenthèses (ouverture et fermeture)
     parEntr=phrase_sans_op.count('(')
     parFerm=phrase_sans_op.count(')')
     if parEntr!=parFerm:
-        print "Votre phrase contient "+str(parEntr)+" ouverture(s) de parenthèse pour "+str(parFerm)+" fermeture(s) !"
+        print("Votre phrase contient "+str(parEntr)+" ouverture(s) de parenthèse pour "+str(parFerm)+" fermeture(s) !")
         return None
 
     # 2.3-Vérification qu'un opérateur ne colle pas l'intérieur d'une parenthèse
     if '(|' in phrase or '|)' in phrase:
-        print 'Un opérateur colle l\'intérieur d\'une parenthèse !'
+        print('Un opérateur colle l\'intérieur d\'une parenthèse !')
         return None
     
     
@@ -46,12 +46,12 @@ def ou(liste):
     dev=[]
     for e in range(len(liste)):
         dev.append(liste[e].split("|ET|"))
-    print dev
+    print(dev)
     
     # 3-On vérifie qu'aucun des éléments de OU n'est sous-ensemble de l'un des autres (ex: a|ET|b inclus dans a|ET|b|ET|c -> la deuxième liste est supprimée)
     simpl=dev[:]
     for e in range(len(dev)):
-        
+        # A COMPLETER
+        None
     
-    return liste
-    
+    return liste    
